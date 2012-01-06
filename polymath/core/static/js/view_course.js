246b4b6fb2b57646e8d5c91190cc377de6f0fd4b
Harish Venkatesan <harishv33@gmail.com>
2012-01-05 22:20:28 -0800

    removed all inline css and js

--- polymath/core/static/js/view_course.js
+++ polymath/core/static/js/view_course.js
@@ -2,6 +2,56 @@
 
 $(document).ready(function(){
 	
+	// set the current value of the progress bar
+	$('#progressbar').progressbar({
+		value: (completed_lessons/lessons) *100			
+	});
+	
+	////
+	
+	$('.checkb').click(function(){ // when the done button is clicked...
+		
+		if ( $(this).hasClass('done')){ // don't do anything if it's already done
+		
+		} else {
+			
+			$(this).toggleClass("done"); // mark the checkbox as done
+	
+			var val = $("#progressbar").progressbar("option", "value"); // set current value of progress bar
+		
+			var pGress = setInterval(function() { // loop to animate progress bar action
+		        var pVal = $('#progressbar').progressbar('option', 'value');
+		        var pCnt = !isNaN(pVal) ? (pVal + 1) : (completed_lessons/lessons) *100;
+		        if (pCnt >= val + (100/lessons) ) { //animate progress bar to number of currently completed lesson
+		            clearInterval(pGress);
+		        } else {
+		            $('#progressbar').progressbar({value: pCnt});
+		        }
+		    }, 10);
+
+
+			lesson_id = $(this).closest('.lessonBlock').find('.lesson_id').val();
+	        	$.post(
+	            	from_server['complete_lesson_url'],
+	            	{ lesson_id: lesson_id },
+	            	function(response) {
+	    		//            alert(response['result_message']);
+	                if( response['complete_successful'] ) {
+						$("#numbercompleted").text(+($("#numbercompleted").text()) + 1); // increment the number of completed lessons
+						$(this).closest('.lessonBlock').toggleClass("completedBlock");
+	               }
+	            }
+	        );
+
+			} // endif
+			
+		}); // end done animation
+	
+	
+	
+	
+	
+	///
 
     
     $('#test_btn').click(function() {
@@ -29,7 +79,7 @@ $(document).ready(function(){
 
     });
 
-
+	// lesson completed
     $('.complete_lesson_lnk').click(function() {
         complete_spn = $(this).parent();
         lesson_id = $(this).closest('.lessonBlock').find('.lesson_id').val();
@@ -50,15 +100,23 @@ $(document).ready(function(){
     });
 
 
-
-			$('.lessonBlock').mouseenter(function(){
-				$(this).toggleClass("highlight");
-			});
+	//toggle lessonBlock view
+	$('.lessonBlock').mouseenter(function(){
+		$(this).toggleClass("highlight");
+	});
 			
-			$('.lessonBlock').mouseleave(function(){
-				$(this).toggleClass("highlight");
-			});
+	$('.lessonBlock').mouseleave(function(){
+		$(this).toggleClass("highlight");
+	});
 
+	//tooltips
+	$('.checkb').tipsy({fade: false, gravity: 's', opacity:0.6});
+
+	$('.checkb.done').tipsy({fade: false, gravity: 's', opacity:0.6});
+
+	$('.takecourse').tipsy({fade: false, gravity: 'e', opacity:0.6});
+
+	$('.lessonname').tipsy({fade: false, gravity: 'w', opacity:0.6, offset:10});
 
     
 }); 
