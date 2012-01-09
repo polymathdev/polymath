f95bf50b7955856a4c1481e3f08d2e061694ac4f
Harish Venkatesan <harishv33@gmail.com>
2012-01-08 16:42:23 -0800

    settings file change

--- polymath/settings_dev.py
+++ polymath/settings_dev.py
@@ -39,7 +39,7 @@ elif host == 'harish-venkatesans-macbook.local':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-            'NAME': 'newdb',                      
+            'NAME': 'pm_db_psql',                      
             'USER': 'postgres',                      
             'PASSWORD': 'stitch1',                 
             'HOST': 'localhost',                     
