ba196eccc27b408f36f357d59fff92924500c9b3
Harish Venkatesan <harishv33@gmail.com>
2012-01-24 18:32:39 -0800

    welcome page and style changes

--- polymath/core/static/js/about_page.js
+++ polymath/core/static/js/about_page.js
@@ -12,6 +12,8 @@ $(document).ready(function(){
 	$(".aboutsidebar ul li:eq(1)").addClass('selected');
 	} else if (current_page.match(/howitworks/)) {
 	$(".aboutsidebar ul li:eq(2)").addClass('selected');
+	}  else if (current_page.match(/welcome/)) {
+	$(".aboutsidebar ul li:eq(2)").addClass('selected');
 	} else { // don't mark any nav links as selected
 	$("ul#navigation li").removeClass('selected');
 	};
