3f4b78b7875e80f50fd7b851b5367c962bdbe43c
Harish Venkatesan <harishv33@gmail.com>
2012-01-29 13:23:17 -0800

    new how it works page

--- polymath/core/static/js/about_page.js
+++ polymath/core/static/js/about_page.js
@@ -9,11 +9,11 @@ $(document).ready(function(){
 	if (current_page.match(/about/)) {
 	$(".aboutsidebar ul li:eq(0)").addClass('selected');
 	} else if (current_page.match(/beliefs/)) {
-	$(".aboutsidebar ul li:eq(1)").addClass('selected');
+	$(".aboutsidebar ul li:eq(2)").addClass('selected');
 	} else if (current_page.match(/howitworks/)) {
-	$(".aboutsidebar ul li:eq(2)").addClass('selected');
+	$(".aboutsidebar ul li:eq(1)").addClass('selected');
 	}  else if (current_page.match(/welcome/)) {
-	$(".aboutsidebar ul li:eq(2)").addClass('selected');
+	$(".aboutsidebar ul li:eq(1)").addClass('selected');
 	} else { // don't mark any nav links as selected
 	$("ul#navigation li").removeClass('selected');
 	};
