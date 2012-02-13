9a50279944360bc807965e8ca7b89bc0b52bcfa1
Harish Venkatesan <harishv33@gmail.com>
2012-02-12 19:30:43 -0800

    submit lesson page

---
+++ polymath/core/static/js/submit_lesson.js
@@ -0,0 +1,23 @@
+head.ready(function(){
+	
+
+
+
+$(document).ready(function(){
+
+
+	$("select").sb();
+	
+	$('#tags_input').tagsInput({
+		'width':'275px',
+		'height':'55px',
+		'defaultText':'Add a tag',
+	});
+	
+	
+}); 
+
+
+
+
+});
