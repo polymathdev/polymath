29963ee36480c3ca42df17a917a1860c9ab6b394
Harish Venkatesan <harishv33@gmail.com>
2012-02-19 15:31:35 -0800

    share lesson form

--- polymath/core/static/js/submit_lesson.js
+++ polymath/core/static/js/submit_lesson.js
@@ -8,11 +8,54 @@ $(document).ready(function(){
 
 	$("select").sb();
 	
-	$('#tags_input').tagsInput({
+	$('#id_tags').tagsInput({
 		'width':'275px',
-		'height':'55px',
+		'height':'70px',
 		'defaultText':'Add a tag',
+		'onAddTag': updatehiddendiv,			// when a new tag is added, update the hidden div that the validate function checks
 	});
+	
+	function updatehiddendiv(){
+		var value = $('#id_tags').val();
+		$("#tagsexist").val(value);
+		$("#submit_lesson_form").validate().element("#tagsexist");
+	}
+	
+	
+	$("#submitlesson").click(function(){
+		updatehiddendiv();
+		
+	});
+
+
+	$("#submit_lesson_form").validate({
+		submitHandler: function(form){
+			$('#submit_lesson_form :submit').attr('disabled','true');
+			form.submit();
+		},
+		rules: {
+			name: {
+				required: true,
+				maxlength: 40
+			},
+			description: {
+				required: true
+			},
+			category: {
+				required: true
+			}, 
+			link :{
+				required: true
+			}, 
+			type: {
+				required: true
+			}, 
+			tagsExist:{
+				required: true
+			}
+		}, 		
+	});
+	
 	
 	
 }); 
