09709200e0b2ea2f913f868a675123b4c926479c
Harish Venkatesan <harishv33@gmail.com>
2012-02-24 16:55:35 -0800

    updates to how it works for onboarding, and front end for popular tags

---
+++ polymath/core/static/js/howitworks.js
@@ -0,0 +1,18 @@
+head.ready(function(){
+
+
+$(document).ready(function(){
+
+	$('#coda-slider-1').codaSlider({
+				dynamicTabs: false, 
+				dynamicArrows: false,
+				slideEaseDuration: 500,
+			});
+		
+
+		
+});
+
+
+});
+
