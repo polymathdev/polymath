cfdaec0b7763c3f23dc07ff8cda8c3a9f7508bba
Harish Venkatesan <harishv33@gmail.com>
2012-02-25 02:50:05 -0800

    updates to how it works + footer + fixed alignment issue on course page

--- polymath/core/static/js/howitworks.js
+++ polymath/core/static/js/howitworks.js
@@ -6,10 +6,36 @@ $(document).ready(function(){
 	$('#coda-slider-1').codaSlider({
 				dynamicTabs: false, 
 				dynamicArrows: false,
-				slideEaseDuration: 500,
+				slideEaseDuration: 200,
 			});
 		
+			$("#nextbutton").click(function(){
+				$(this).hide();
+				setTimeout(function(){
+				     var height = $("#coda-slider-1").height();
+					console.log("height of panel is %s", height);
+					height = height/2;
+					console.log("new height is %s", height);
+					$("#nextbutton").css("margin-top", height);
+					$("#nextbutton").show();
+				},800);
+			});
+			
+			
+			$(".previewimages a").click(function(){
+				$("nextbutton").hide();
+				setTimeout(function(){
+				     var height = $("#coda-slider-1").height();
+					console.log("height of panel is %s", height);
+					height = height/2;
+					console.log("new height is %s", height);
+					$("#nextbutton").css("margin-top", height);
+					$("#nextbutton").show();
+				},800);
 
+				
+			});
+	
 		
 });
 
