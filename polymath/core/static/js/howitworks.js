f12eddb601d0b0a65f97222d201e7c0f446fdbb4
Harish Venkatesan <harishv33@gmail.com>
2012-02-26 20:51:46 -0800

    welcome walkthrough

--- polymath/core/static/js/howitworks.js
+++ polymath/core/static/js/howitworks.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
 	
 	$("#dim").css("height", $(document).height());
 	
+	$('#smelly').appear(function() {
+		$("#howitworkscall").delay(1800).fadeIn();
+		$(".messages").delay(1500).fadeOut();
+		$("#nextbutton").delay(1500).fadeOut();
+		$("#coda-nav-1").delay(1500).fadeIn();
+	});
+	
+	
 	$('#coda-slider-1').codaSlider({
 				dynamicTabs: false, 
 				dynamicArrows: false,
